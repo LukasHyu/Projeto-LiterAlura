6b06bd2685f75198b778f158b9fb6e3f20eb2cbd
LukasHyu <lukas.lex@hotmail.com>
2025-07-29 10:57:50 -0300

    Projeto Inicial
    Projeto inicial somente com os arquivos gerados pelo spring start

---
+++ LiterAlura/src/main/java/com/example/LiterAtura/LiterAturaApplication.java
@@ -0,0 +1,13 @@
+package com.example.LiterAtura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiterAturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiterAturaApplication.class, args);
+	}
+
+}
